1af9dc42bc3cd016c74feec8da7e1d96df6ffbd4
Matěj Šusta <matej.susta@tieto.com>
2016-12-01 22:47:07 +0100

    Update bcm2835 to 1.50

--- build_rpi.sh
+++ build_rpi.sh
@@ -4,7 +4,7 @@ USER_NAMESERVER="10.120.200.63"
 USER_APT_SERVER="http://mirrordirector.raspbian.org/raspbian/"
 
 #Constants
-checksum_sha512_bcm2835="d1f006614cb1f95e1a6c33081e3a932984d54b00f535d6b7ccde774ff59ac40a858c5f7822e6f5103d207cacf8ee0a8325c2d36673607b54b4fdcdb090209c25"
+checksum_sha512_bcm2835="94887f04616c3bea4e2cb5d265068208958777feffb88ccbb33a03e9d8b8a7f06f06d9744dae45cd33d24f17a29ae1fff437a23521084667a57de8208855105c"
 py_spidev_commit="4f3f1dea5d4828c387e8822aa6bf3920a4119c64"
 
 # Configure default gateway and nameserver - Change to suit your network
@@ -16,26 +16,26 @@ sudo sh -c 'echo "Acquire::http::Proxy \"http://192.168.137.3:3128\";" > /etc/ap
 
 #Download BCM2835 library
 echo "Downloading bcm2835"
-wget www.airspayce.com/mikem/bcm2835/bcm2835-1.44.tar.gz -O bcm2835-1.44.tar.gz || { echo "Failed to download bcm2835 library" && exit; }
+wget www.airspayce.com/mikem/bcm2835/bcm2835-1.50.tar.gz -O bcm2835-1.50.tar.gz || { echo "Failed to download bcm2835 library" && exit; }
 
 #Verify the SHA512 Hash
-bcm2835_sha512=$(sha512sum bcm2835-1.44.tar.gz | head -c 128)
+bcm2835_sha512=$(sha512sum bcm2835-1.50.tar.gz | head -c 128)
 
 if [ "$bcm2835_sha512"=="$checksum_sha512_bcm2835" ]
 	then
 		echo "Checksum verification PASSED"
 	else
-		echo "Checksum verification FAILED. Something went wrong while downloading bcm2835-1.44.tar.gz"
+		echo "Checksum verification FAILED. Something went wrong while downloading bcm2835-1.50.tar.gz"
 		echo "Exiting..."
 		exit
 fi
 
 echo "Extracting the downloaded file"
-tar -zxvf bcm2835-1.44.tar.gz
+tar -zxvf bcm2835-1.50.tar.gz
 
 #Compile
 echo "Building BCM2835 library"
-cd bcm2835-1.44
+cd bcm2835-1.50
 ./configure
 sudo make check
 sudo make install
